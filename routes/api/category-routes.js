62c31b0653e910aa7aa11536b7ec7fae75656563
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-07-06 11:43:16 -0500

    routes added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,16 +5,43 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [{ model: Location, through: Trip, as: 'category_id' }]
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const CategoryData = await Category.findAll();
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
   // be sure to include its associated Products
+}
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+    try {
+      const CreateData = await Category.create(req.body);
+      res.status(200).json(CreateData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +50,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const locationData = await Location.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
